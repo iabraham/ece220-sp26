a8fbd1369a754c4af5941a99363d6be19a2e1c67
Ivan Abraham <ivan.abrms@gmail.com>
2026-03-10 15:03:05 -0500

    Prelecture on 03/10

---
+++ lec14_0310/logging.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  fprintf(stdout, "Normal output1\n");
+  fprintf(stdout, "Normal output2\n");
+  fprintf(stderr, "Error1 \n");
+  fprintf(stdout, "Normal output3\n");
+  fprintf(stderr, "Warning1\n");
+}
